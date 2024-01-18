7e999462f256218281bbad45ea46a504e41a2a4b
Dennis Dunn <ansofive@gmail.com>
2024-01-18 12:29:08 -0500

    Update the manifest with permissions info.

--- PicoLifeApp/Views/DevicePage.xaml.cs
+++ PicoLifeApp/Views/DevicePage.xaml.cs
@@ -1,15 +1,24 @@
-
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
 
 namespace PicoLife.Views;
 
 public partial class DevicePage : ContentPage
 {
-	public DevicePage()
-	{
-		InitializeComponent();
+    IBluetoothLE BLE = CrossBluetoothLE.Current;
+    IAdapter BLEAdapter = CrossBluetoothLE.Current.Adapter;
+
+    public DevicePage()
+    {
+        InitializeComponent();
     }
 
     async void OnScanClicked(object sender, EventArgs e)
     {
+        var state = BLE.State;
+
+        var deviceList = new List<IDevice>();
+        BLEAdapter.DeviceDiscovered += (s, a) => deviceList.Add(a.Device);
+        await BLEAdapter.StartScanningForDevicesAsync();
     }
 }
